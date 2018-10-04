320ade1a41548097f1ed45e6adeea981506e4daf
Suheyl Turac <skbsturac@live.com>
2018-10-04 04:39:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,21 +1,24 @@
 def badge_maker(name)
   badge = "Hello, my name is #{name}."
 end
- def batch_badge_creator(name_list)
+
+def batch_badge_creator(name_list)
   badge_list = []
   name_list.each do |name|
     badge_list << "Hello, my name is #{name}."
   end
   badge_list
 end
- def assign_rooms(name_list)
+
+def assign_rooms(name_list)
   welcome_message = []
   name_list.each_with_index do |name,index|
     welcome_message << "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
   welcome_message
 end
- def printer(attendees)
+
+def printer(attendees)
   batch_badge_creator(attendees).each do |person|
     puts person
   end
